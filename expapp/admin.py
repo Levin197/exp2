4854df8894d31fdb82a8fff646ab916b4e7e8e28
Levin197 <levinpaul197@gmail.com>
2025-03-17 11:54:28 +0530

    orm

---
+++ expapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from expapp.models import cienima
+admin.site.register(cienima)
+
---
+++ expapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+class cienima(models.Model):
+    name=models.CharField(max_length=100)
+    location=models.CharField(max_length=100)
+    phone=models.CharField(max_length=100)
+    email=models.EmailField(max_length=100)
+    image=models.ImageField(upload_to='cinema/')
+    description=models.TextField()
+    def __str__(self):
+        return self.name
+
